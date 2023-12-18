94f45deeec39fdb0c9034f6e7c4fcb3831515cac
Cyadine <61088560+zvbt@users.noreply.github.com>
2023-12-18 22:34:21 +0100

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,19 +2,9 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 
 local config = {}
--- Use config builder object if possible
 if wezterm.config_builder then config = wezterm.config_builder() end
 config.default_prog = { 'C:/Program Files/Git/bin/sh', '-l' }
 
--- ssh
-config.ssh_domains = {
-  {
-    name = 'vps',
-    remote_address = '192.168.1.60',
-    username = 'root',
-  },
-}
--- Settings
 config.color_scheme = "Catppuccin Frappe"
 config.font = wezterm.font_with_fallback({
   { family = "Cascadia Mono",  scale = 1.2 }
@@ -24,14 +14,10 @@ config.window_decorations = "RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 3000
 config.default_workspace = "home"
-
--- Dim inactive panes
 config.inactive_pane_hsb = {
   saturation = 0.24,
   brightness = 0.5
 }
-
--- Keys
 
 config.keys = {
   
@@ -44,34 +30,23 @@ config.mouse_bindings = {
 
 }
 
-
--- Tab bar
--- I don't like the look of "fancy" tab bar
 config.use_fancy_tab_bar = false
 config.status_update_interval = 1000
 wezterm.on("update-right-status", function(window, pane)
-  -- Workspace name
+
   local stat = window:active_workspace()
-  -- It's a little silly to have workspace name all the time
-  -- Utilize this to display LDR or current key table name
   if window:active_key_table() then stat = window:active_key_table() end
   if window:leader_is_active() then stat = "LDR" end
 
-  -- Current working directory
+
   local basename = function(s)
-    -- Nothign a little regex can't fix
     return string.gsub(s, "(.*[/\\])(.*)", "%2")
   end
   local cwd = basename(pane:get_current_working_dir())
-  -- Current command
   local cmd = basename(pane:get_foreground_process_name())
-
-  -- Time
   local time = wezterm.strftime("%H:%M")
-
-  -- Let's add color to one of the components
+    
   window:set_right_status(wezterm.format({
-    -- Wezterm has a built-in nerd fonts
     { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
     { Text = " | " },
     { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
