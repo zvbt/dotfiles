ee0307b4d7e476c16718424348129bdc5f02a510
zvbt <cyadine@skiff.com>
2024-03-16 12:04:35 +0100

    Added GlazeWM installation

--- install.sh
+++ install.sh
@@ -1,8 +1,31 @@
 log_file=~/config_logs.txt
 config=~/.config
 
-# Alacritty
-if winget list 2>/dev/null | grep -q Alacritty; then
+# handle_error() {
+#     local exit_code=$?
+#     echo -e "\033[0;31m An error occurred. (try running this script as admin) \033[0m"
+#     exit $exit_code
+# }
+# trap 'handle_error' ERR
+
+# Scoop & winget check
+if scoop -v 2>/dev/null | grep -q Scoop; then
+    echo "Scoop is already installed." | tee -a $log_file
+    scoop=true
+else
+    echo "Please install Scoop https://scoop.sh/" | tee -a $log_file
+fi
+if winget 2>/dev/null | grep -q winget; then
+    echo "Winget is already installed." | tee -a $log_file
+    winget=true
+else
+    echo "Please install Winget https://aka.ms/getwinget" | tee -a $log_file
+fi
+
+
+if [ "$scoop" = true ] && [ "$winget" = true ]; then
+    # Alacritty
+if alacritty --version list 2>/dev/null | grep -q alacritty; then
     echo "Alacritty is already installed."| tee -a $log_file
     cp -r "./alacritty" $APPDATA
     echo "Moving the config to $APPDATA/alacritty" | tee -a $log_file
@@ -15,15 +38,6 @@ else
     echo "Moving the config to $APPDATA/alacritty" | tee -a $log_file
     cp -r "./alacritty" $APPDATA
     echo "Alacritty done." | tee -a $log_file
-fi
-
-# Scoop
-if scoop -v 2>/dev/null | grep -q Scoop; then
-    echo "Scoop is already installed." | tee -a $log_file
-else
-    echo "Running Scoop installer in a powershell window" | tee -a $log_file
-    powershell Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-    powershell -Command "irm get.scoop.sh | iex"
 fi
 
 # MPV
@@ -40,6 +54,36 @@ else
     cp -r "./mpv"/* $USERPROFILE/scoop/apps/mpv/current/portable_config/
     echo "mpv done." | tee -a $log_file
 fi
+
+
+# GlazeWM
+if scoop list 2>/dev/null | grep -q glazewm; then
+    echo "GlazeWM is already installed." | tee -a $log_file
+    echo "Moving the config to $USERPROFILE/.glaze-wm/" | tee -a $log_file
+    cp -r "./glaze-wm"/* $USERPROFILE/.glaze-wm/
+    echo "Creating a startmenu shortcut & auto startup..." | tee -a $log_file
+    powershell -Command "New-Item -ItemType SymbolicLink -Path '$APPDATA\Microsoft\Windows\Start Menu\Programs' -Name "GlazeWM.lnk" -Value '$USERPROFILE/scoop/apps/glazewm/current/GlazeWM.exe'"
+    powershell -Command "New-Item -ItemType SymbolicLink -Path '$APPDATA\Microsoft\Windows\Start Menu\Programs\Startup' -Name "GlazeWM.lnk" -Value '$USERPROFILE/scoop/apps/glazewm/current/GlazeWM.exe'"
+    echo "GlazeWM done." | tee -a $log_file
+else
+    echo "GlazeWM is not installed." | tee -a $log_file
+    echo "Installing GlazeWM..." | tee -a $log_file
+    scoop install glazewm
+    echo "Successfully installed GlazeWM." | tee -a $log_file
+    echo "Moving the config to $USERPROFILE/.glaze-wm/" | tee -a $log_file
+    mkdir $USERPROFILE/.glaze-wm/
+    cp -r "./glaze-wm"/* $USERPROFILE/.glaze-wm/
+    echo "Creating a startmenu shortcut & auto startup..." | tee -a $log_file
+    powershell -Command "New-Item -ItemType SymbolicLink -Path '$APPDATA\Microsoft\Windows\Start Menu\Programs' -Name "GlazeWM.lnk" -Value '$USERPROFILE/scoop/apps/glazewm/current/GlazeWM.exe'"
+    powershell -Command "New-Item -ItemType SymbolicLink -Path '$APPDATA\Microsoft\Windows\Start Menu\Programs\Startup' -Name "GlazeWM.lnk" -Value '$USERPROFILE/scoop/apps/glazewm/current/GlazeWM.exe'"
+    echo "GlazeWM done." | tee -a $log_file
+fi
+
+else
+    echo "Please make sure to install winget & scoop." | tee -a $log_file
+fi
+
+
 
 
 #==============
