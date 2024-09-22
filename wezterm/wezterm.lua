c26a0be51af0047602e66393f432a9d0341acb61
Cyadine <61088560+zvbt@users.noreply.github.com>
2024-09-22 16:08:06 +0200

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,18 +2,19 @@ local wezterm = require 'wezterm'
 
 return {
   default_prog = { "C:\\Program Files\\Git\\bin\\bash.exe", "--login" },
+  default_cwd = os.getenv("HOME") or "C:\\Users\\cyadine",
 
   font = wezterm.font_with_fallback({
     "Maple Mono NF",
+    "Mononoki Nerd Font"
   }),
   font_size = 12.0,
 
-  enable_wayland = false,
+  enable_wayland = true,
   term = "xterm-256color",
 
-  color_scheme = "Gruvbox dark, pale (base16)",
+  color_scheme = "GruvboxDark",
   window_close_confirmation = "NeverPrompt",
-
 
   window_padding = {
     left = 5,
@@ -22,26 +23,70 @@ return {
     bottom = 0,
   },
 
-  enable_tab_bar = false,
-  window_decorations = "TITLE | RESIZE",
-  window_background_opacity = 0,
-  win32_system_backdrop = 'Mica',
+  enable_tab_bar = true,
+  window_decorations = "RESIZE",
+  window_background_opacity = 1,
+  win32_system_backdrop = 'Disable',
   enable_scroll_bar = false,
+
+
+  colors = {
+    tab_bar = {
+      background = "#282828",
+
+      active_tab = {
+        bg_color = "#458588",
+        fg_color = "#ebdbb2",
+        intensity = "Bold",
+        italic = false,
+        underline = "None",
+      },
+
+      inactive_tab = {
+        bg_color = "#3c3836",
+        fg_color = "#a89984",
+      },
+
+      inactive_tab_hover = {
+        bg_color = "#504945",
+        fg_color = "#ebdbb2",
+      },
+
+      new_tab = {
+        bg_color = "#3c3836",
+        fg_color = "#a89984",
+      },
+
+      new_tab_hover = {
+        bg_color = "#504945",
+        fg_color = "#ebdbb2",
+      },
+    },
+  },
 
   -- Keybindings
   keys = {
+
+    -- close tab  
+    {
+      key = "q", -- ctrl+shift+w
+      mods = "ALT",
+      action = wezterm.action.CloseCurrentTab({ confirm = false }),
+    },
+
+    -- ssh config
     {
       key = "!", -- ctrl+shift+1
       mods = "CTRL|SHIFT",
       action = wezterm.action.SpawnCommandInNewTab {
-        args = { "ssh", "root@@*****"" },
+        args = { "ssh", "root@192.168.1.32" },
       },
     },
     {
       key = "@", -- ctrl+shift+2
       mods = "CTRL|SHIFT",
       action = wezterm.action.SpawnCommandInNewTab {
-        args = { "ssh", "root@*****" },
+        args = { "ssh", "root@94.156.35.151" },
       },
     },
     {
@@ -51,5 +96,18 @@ return {
         args = { "wsl", "-d", "Arch" },
       },
     },
+  },
+
+  mouse_bindings = {
+    {
+      event = { Down = { streak = 1, button = "Right" } },
+      mods = "NONE",
+      action = wezterm.action.PasteFrom("Clipboard"),
+    },
+    {
+      event = { Down = { streak = 1, button = "Right" } },
+      mods = "CTRL",
+      action = wezterm.action.CopyTo("ClipboardAndPrimarySelection"),
+    },    
   },
 }
